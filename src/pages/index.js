a040566944802f5793a794659778fe9cb44bda94
m0slah <muhammedslahqadir@gmail.com>
2023-03-31 14:14:58 +0300

    add mainHeader

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import React, {useContext} from "react";
 import Home from "@/Components/Home/Home";
 import Login from "@/Components/Login/Login";
 import AuthContext from "@/Components/store/auth-context";
+import MainHeader from "@/Components/Header/MainHeader";
 
 function index() {
   const ctx=useContext(AuthContext);
 
   return (
     <React.Fragment>
+      <MainHeader/>
       <main>
         {!ctx.isLoggedIn && <Login />}
         {ctx.isLoggedIn && <Home />}
