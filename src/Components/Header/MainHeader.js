0f442d2daa3b0cb09fef97f6a9b65ae5227d985e
m0slah <muhammedslahqadir@gmail.com>
2023-03-31 14:21:21 +0300

    add navigation component

--- src/Components/Header/MainHeader.js
+++ src/Components/Header/MainHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import classes from './MainHeader.module.css';
+import Navigation from './Navigation';
 
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
+          <h1>A Typical Page</h1>
+          <Navigation/>
     </header>
   );
 };
